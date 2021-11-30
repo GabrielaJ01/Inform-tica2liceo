4b880ba1afaad301bc98a1823e91bdd99a50755b
Gabriela Jaramillo <gabriela.jaramillo.ruiz@gmail.com>
2021-11-30 15:28:48 -0500

    Tarea1.2

--- LChapter2/condicionales2/ejercicio7.js
+++ LChapter2/condicionales2/ejercicio7.js
@@ -1,2 +1,10 @@
-let velocidadInicial = (prompt("Ingrese la velocidad inicial en m/s"))
-let 
+let velocidadInicial = Number(prompt("Ingrese la velocidad inicial en m/s"))
+let altura = Number(prompt("A qué altura desea llegar en metros?"))
+let tiempo = (0-velocidadInicial)/(0-9.8)
+let desplazamiento = (velocidadInicial)-(9.8*tiempo*tiempo)/2
+let resultado = document.querySelector ("#resultado")
+if (desplazamiento >= altura){
+resultado.innerHTML = ("El proyectil logra llegar a la altura deseada")
+}else{
+    resultado.innerHTML = ("El proyectil no logra llegar a la altura deseada")
+}
