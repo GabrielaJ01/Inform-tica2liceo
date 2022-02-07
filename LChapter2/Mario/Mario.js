5473993b4f30d73179a1906b4ac6f8cf36aa15f8
GabrielaJ01 <91990192+GabrielaJ01@users.noreply.github.com>
2022-02-07 16:48:58 -0500

    Add files via upload

---
+++ LChapter2/Mario/Mario.js
@@ -0,0 +1,23 @@
+function piso (nPisos, altura){
+    // To do. Deben poner el número correcto de espacios y de numerales de acuerdo als nPiso y a la altura
+    texto = "<p>"
+    for (i=0; i< altura-nPisos; i++){
+        texto = texto + "&nbsp"
+    }
+    for (i=0; i<nPisos; i++){
+        texto = texto + "#";
+    }
+    texto = texto+"</p>";
+    return texto
+}
+let nPisos;
+do { //Lo que se hace
+    altura = parseInt(prompt("Ingrese la altura de sus escaleras"))
+} while (altura%1 !=0 || altura >= 10 || isNaN(altura)); //La condicion para que se repita  
+let contenido = ""
+for (let i = 1; i <= altura; i++){
+    contenido = contenido + piso(i, altura)
+}
+
+let contenedor = document.querySelector("#contenedor")
+contenedor.innerHTML = contenido;
