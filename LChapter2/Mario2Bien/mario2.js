d01bb1712cd38883ab6c4e4f48c8b3e90e52a2d3
GabrielaJ01 <91990192+GabrielaJ01@users.noreply.github.com>
2022-02-08 15:05:34 -0500

    Add files via upload

---
+++ LChapter2/Mario2Bien/mario2.js
@@ -0,0 +1,31 @@
+function piso (nPiso, altura){
+    nEspacios = altura - nPiso
+    nNumerales = nPiso
+    // To do. Deben poner el número correcto de espacios y de numerales de acuerdo als nPiso y a la altura
+    texto = "<p>"
+    for (i=0; i<nEspacios; i++){
+        texto = texto + "&nbsp"
+    }
+    for (i=0; i<nNumerales; i++){
+        texto = texto + "#";
+    }
+    texto = texto + "&nbsp"
+    for (let i = 0; i < nNumerales; i++) {
+        texto = texto + "#";
+    }
+    texto = texto+"</p>";
+    return texto
+}
+
+let nPisos;
+do {
+    nPisos = parseInt(prompt("Ingrese el número de pisos que desee, puede ser un número del 1 al 10"))
+} while (isNaN(nPisos) || nPisos<=0 || nPisos >= 10 );
+let contenido = ""
+for (let i=1; i <= nPisos; i++) {
+    contenido = contenido + piso(i, nPisos);
+    
+}
+
+let contenedor = document.querySelector("#contenedor")
+contenedor.innerHTML = contenido;
