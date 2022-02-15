db273b3511b6c390fbdefea59e6d2cf9252bb6c7
GabrielaJ01 <91990192+GabrielaJ01@users.noreply.github.com>
2022-02-15 15:24:53 -0500

    Add files via upload

---
+++ LChapter2/edad.js
@@ -0,0 +1,9 @@
+nombres = ["Arámbula","Anita","Dani","Yeidi","Gabyloca","Chechito","Tomycrack","Pulga","Wana","Mari","Villa","Francisco","Jorgito","Mate","Jacobo","Castellanos","Jose"]
+edades = []
+suma=0
+for (let i = 0; i <17; i++) {
+ do {
+    parseInt(edades.push(Number(prompt(`Ingrese la edad de ${nombres[i++]}`))))
+ } while (isNan (edad) || edad<20 || edad>10);
+}
+ 
